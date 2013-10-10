4ff0d79d8097bc06ff1258711a94adf88d6a377f
tpluscode <tomasz@t-code.pl>
2013-10-10 14:34:58 +0000

    remove redundant comments

--- RomanticWeb/Converters/XsdConverterBase.cs
+++ RomanticWeb/Converters/XsdConverterBase.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using RomanticWeb.Entities;
 
 namespace RomanticWeb.Converters
 {
@@ -13,19 +12,7 @@ namespace RomanticWeb.Converters
 
         public bool CanConvert(Uri dataType)
         {
-            ////IEntity entity=null;
-            ////from x in new List<IComposition>()
-            ////where x.ValidForType.Any(item => item==((OntologyAccessor)entity["magi"])["DataStructure"])
-            ////select x;
-
-            ////entity.rdf.type
-
             return SupportedTypes.Contains(dataType,new AbsoluteUriComparer());
         }
     }
-
-    ////interface IComposition
-    ////{
-    ////    IEnumerable<Uri> ValidForType { get; }
-    ////}
 }

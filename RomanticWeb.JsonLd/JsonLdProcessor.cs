619417fb413e7eaecbfdf8c899d789ced07b1c7b
tpluscode <tomasz@t-code.pl>
2014-02-13 16:38:09 +0000

    some refactoring of JSON-LD processor

--- RomanticWeb.JsonLd/JsonLdProcessor.cs
+++ RomanticWeb.JsonLd/JsonLdProcessor.cs
@@ -9,59 +9,64 @@ namespace RomanticWeb.JsonLd
 {
     public class JsonLdProcessor : IJsonLdProcessor
     {
-        internal const string Id = "@id";
-        internal const string Language = "@language";
-        internal const string Value = "@value";
-        internal const string Context = "@context";
-        internal const string Graph = "@graph";
-        internal const string Type = "@type";
-        internal const string List = "@list";
-        private Node nodeType = Node.ForLiteral("@type");
-        private Node rdfType;
-        private Node type = Node.ForUri(Rdf.type);
-        private Node first = Node.ForUri(Rdf.first);
-        private Node rest = Node.ForUri(Rdf.rest);
-        private Node nil = Node.ForUri(Rdf.nil);
+        /// <summary>
+        /// @id
+        /// </summary>
+        private const string Id = "@id";
+
+        /// <summary>
+        /// @language
+        /// </summary>
+        private const string Language = "@language";
+
+        /// <summary>
+        /// @value
+        /// </summary>
+        private const string Value = "@value";
+
+        /// <summary>
+        /// @context
+        /// </summary>
+        private const string Context = "@context";
+
+        /// <summary>
+        /// @graph
+        /// </summary>
+        private const string Graph = "@graph";
+
+        /// <summary>
+        /// @type
+        /// </summary>
+        private const string Type = "@type";
+
+        /// <summary>
+        /// @list
+        /// </summary>
+        private const string List = "@list";
+
+        /// <summary>
+        /// rdf:type
+        /// </summary>
+        private readonly Node _rdfType = Node.ForUri(Rdf.type);
+
+        /// <summary>
+        /// rdf:first
+        /// </summary>
+        private readonly Node _rdfFirst = Node.ForUri(Rdf.first);
+
+        /// <summary>
+        /// rdf:rest
+        /// </summary>
+        private readonly Node _rdfRest = Node.ForUri(Rdf.rest);
+
+        /// <summary>
+        /// rdf:nil
+        /// </summary>
+        private readonly Node _rdfNil = Node.ForUri(Rdf.nil);
         private JArray listInGraph = new JArray();
-        private bool nativeTypes = false;
         private List<Node> nodesInList = new List<Node>();
 
-        public string FromRdf(IEnumerable<EntityQuad> dataset, bool userRdfType = false, bool useNativeTypes = false)
-        {
-            nativeTypes = useNativeTypes;
-            if (!userRdfType)
-            {
-                rdfType = nodeType;
-            }
-            else
-            {
-                rdfType = type;
-            }
-
-            return GetJsonStructure(dataset).ToString();
-        }
-
-        public IEnumerable<EntityQuad> ToRdf(string json)
-        {
-            yield break;
-        }
-
-        public string Flatten(string json, string jsonLdContext)
-        {
-            return json;
-        }
-
-        public string Compact(string json, string jsonLdContext)
-        {
-            return json;
-        }
-
-        public string Expand(string json, JsonLdOptions options)
-        {
-            return json;
-        }
-
-        private JArray GetJsonStructure(IEnumerable<EntityQuad> quads)
+        public string FromRdf(IEnumerable<EntityQuad> quads, bool userRdfType = false, bool useNativeTypes = false)
         {
             IDictionary<JToken, JObject> subjectMap = new Dictionary<JToken, JObject>();
             var dataset = from triple in quads
@@ -71,16 +76,16 @@ namespace RomanticWeb.JsonLd
             foreach (var graph in dataset)
             {
                 var lists = GetListsFromGraph(graph);
-
-                var graphLocal = graph;
+                IGrouping<Node, EntityQuad> graph1 = graph;
                 var distinctSubjects = (from q in graph
-                                        where q.Graph == graphLocal.Key && (!nodesInList.Contains(q.Subject))
+                                        where q.Graph == graph1.Key && (!nodesInList.Contains(q.Subject))
                                         orderby q.Subject.IsBlank ? "_:" + q.Subject.BlankNode : q.Subject.ToString() // todo: remove ordering
                                         select q.Subject).Distinct();
 
                 foreach (var subject in distinctSubjects)
                 {
-                    var entitySerialized = SerializeEntity(subject, graph, graph.Key, lists);
+                    Node subject1 = subject;
+                    var entitySerialized = SerializeEntity(subject, graph.Where(n => n.Subject == subject1), graph.Key, useNativeTypes, userRdfType, lists);
 
                     if (graph.Key != null)
                     {
@@ -104,45 +109,68 @@ namespace RomanticWeb.JsonLd
                 }
             }
 
-            return new JArray(subjectMap.Values);
+            return new JArray(subjectMap.Values).ToString();
         }
 
-        private JObject SerializeEntity(Node subject, IEnumerable<EntityQuad> quads, Node graphName, JObject listsInGraph)
+        public IEnumerable<EntityQuad> ToRdf(string json)
+        {
+            yield break;
+        }
+
+        public string Flatten(string json, string jsonLdContext)
+        {
+            return json;
+        }
+
+        public string Compact(string json, string jsonLdContext)
+        {
+            return json;
+        }
+
+        public string Expand(string json, JsonLdOptions options)
+        {
+            return json;
+        }
+
+        private JObject SerializeEntity(Node subject, IEnumerable<EntityQuad> quads, Node graphName, bool nativeTypes, bool useRdfType, JObject listsInGraph)
         {
             var groups = from quad in quads
                          where quad.Subject == subject && quad.Graph == graphName
                          group quad.Object by quad.Predicate into g
                          select new
                          {
-                             Predicate = (g.Key == type ? rdfType : g.Key),
+                             Predicate = (g.Key == _rdfType ? useRdfType ? _rdfType : Node.ForLiteral(Type) : g.Key),
                              Objects = g
                          }
                              into selection
                              orderby selection.Predicate
                              select selection;
 
-            JObject result = new JObject();
+            var result = new JObject();
             int i = 0;
 
-            foreach (var g in groups)
+            foreach (var objectGroup in groups)
             {
-                JProperty res = g.Predicate.ToString().Replace("\"", String.Empty) == Type ?
-                            new JProperty(
-                                new JProperty(
-                                            rdfType.ToString().Replace("\"", String.Empty),
-                                             new JArray(
-                                                from o in g.Objects
-                                                select o.ToString())))
-                :
-                            new JProperty(
-                                new JProperty(
-                                              g.Predicate.ToString(),
-                                              new JArray(
-                                                     from o in g.Objects
-                                                     select ReturnProperties(o, listsInGraph))));
+                JProperty res;
+                if (objectGroup.Predicate == _rdfType || objectGroup.Predicate == Node.ForLiteral(Type))
+                {
+                    if (useRdfType)
+                    {
+                        res = new JProperty(new JProperty(_rdfType.ToString(), new JArray(from o in objectGroup.Objects select GetProperties(o, nativeTypes, listsInGraph))));
+                    }
+                    else
+                    {
+                        res = new JProperty(new JProperty(Type, new JArray(from o in objectGroup.Objects select o.ToString())));
+                    }
+                }
+                else
+                {
+                    res = new JProperty(new JProperty(objectGroup.Predicate.Uri.ToString(), new JArray(from o in objectGroup.Objects select GetProperties(o, nativeTypes, listsInGraph))));
+                }
+
                 if (i == 0)
                 {
-                    result.AddFirst(new JProperty(Id, subject.IsBlank ? "_:" + subject.BlankNode.ToString() : subject.ToString()));
+                    result.AddFirst(new JProperty(Id, subject.IsBlank ? "_:" + subject.BlankNode : subject.ToString()));
                     i++;
                 }
 
@@ -154,8 +182,8 @@ namespace RomanticWeb.JsonLd
 
         private JObject GetListsFromGraph(IEnumerable<EntityQuad> quads)
         {
-            var lists = quads.Where(q => (q.Subject.IsBlank && q.Predicate == rest && q.Object == nil));
-            JObject locList = new JObject();
+            var lists = quads.Where(q => (q.Subject.IsBlank && q.Predicate == _rdfRest && q.Object == _rdfNil));
+            var locList = new JObject();
             foreach (var list in lists)
             {
                 listInGraph.Clear();
@@ -173,14 +201,14 @@ namespace RomanticWeb.JsonLd
         {
             JObject result = new JObject();
             Node localSubject = list.Subject;
-            int anotherPropertyInList = quads.Where(q => q.Subject == localSubject && q.Predicate != type).Count();
-            var firstValues = quads.Where(q => (q.Subject == localSubject && q.Predicate == first)).Select(q => q.Object);
+            int anotherPropertyInList = quads.Where(q => q.Subject == localSubject && q.Predicate != _rdfType).Count();
+            var firstValues = quads.Where(q => (q.Subject == localSubject && q.Predicate == _rdfFirst)).Select(q => q.Object);
             Node firstValue = firstValues.First();
             Node restValue = list.Object;
             listInGraph.AddFirst(ReturnListProperties(firstValue));
             nodesInList.Add(localSubject);
 
-            IEnumerable<EntityQuad> quad = quads.Where(q => (q.Subject.IsBlank && q.Predicate == rest && q.Object == localSubject));
+            IEnumerable<EntityQuad> quad = quads.Where(q => (q.Subject.IsBlank && q.Predicate == _rdfRest && q.Object == localSubject));
             if (anotherPropertyInList > 2 || firstValues.Count() > 1)
             {
                 if (listInGraph.Count() > 1)
@@ -198,7 +226,7 @@ namespace RomanticWeb.JsonLd
                 }
                 else
                 {
-                    IEnumerable<EntityQuad> firstQuad = quads.Where(q => (q.Subject.IsBlank && q.Predicate == first && q.Object == localSubject));
+                    IEnumerable<EntityQuad> firstQuad = quads.Where(q => (q.Subject.IsBlank && q.Predicate == _rdfFirst && q.Object == localSubject));
                     if (firstQuad.Count() == 1)
                     {
                         listInGraph.First.Remove();
@@ -214,26 +242,26 @@ namespace RomanticWeb.JsonLd
 
         private JObject ReturnListProperties(Node @object)
         {
-            JObject returnObject = new JObject();
+            var returnObject = new JObject();
             if (!@object.IsLiteral)
             {
-                JProperty id = new JProperty(Id, @object.IsBlank ? "_:" + @object.BlankNode.ToString() : @object.ToString());
+                var id = new JProperty(Id, @object.IsBlank ? "_:" + @object.BlankNode : @object.ToString());
                 returnObject.Add(id);
             }
             else
             {
-                JProperty value = new JProperty(Value, @object.ToString().Replace("\"", String.Empty));
+                var value = new JProperty(Value, @object.ToString().Replace("\"", String.Empty));
                 returnObject.Add(value);
 
                 if (@object.Language != null)
                 {
-                    JProperty language = new JProperty(Language, @object.Language.ToString().Replace("\"", String.Empty));
+                    var language = new JProperty(Language, @object.Language.Replace("\"", String.Empty));
                     returnObject.Add(language);
                 }
 
                 if (@object.DataType != null)
                 {
-                    JProperty localType = new JProperty(Type, @object.DataType.ToString().Replace("\"", String.Empty));
+                    var localType = new JProperty(Type, @object.DataType.ToString().Replace("\"", String.Empty));
                     returnObject.Add(localType);
                 }
             }
@@ -241,14 +269,14 @@ namespace RomanticWeb.JsonLd
             return returnObject;
         }
 
-        private JObject ReturnProperties(Node @object, JObject lists)
+        private JObject GetProperties(Node @object, bool nativeTypes, JObject lists)
         {
-            JObject returnObject = new JObject();
+            var returnObject = new JObject();
             if (!@object.IsLiteral)
             {
-                if (@object == nil)
+                if (@object == _rdfNil)
                 {
-                    JProperty listProperty = new JProperty(List, new JArray());
+                    var listProperty = new JProperty(List, new JArray());
                     returnObject.Add(listProperty);
                 }
                 else
@@ -256,14 +284,14 @@ namespace RomanticWeb.JsonLd
                     if (lists.Property(@object.ToString()) != null)
                     {
                         var localList = lists.Property(@object.ToString());
-                        if ((object)localList != null)
+                        if (localList != null)
                         {
                             returnObject.Add(new JProperty(List, localList.Value));
                         }
                     }
                     else
                     {
-                        JProperty id = new JProperty(Id, @object.IsBlank ? "_:" + @object.BlankNode.ToString() : @object.ToString());
+                        var id = new JProperty(Id, @object.IsBlank ? "_:" + @object.BlankNode : @object.ToString());
                         returnObject.Add(id);
                     }
                 }
@@ -272,12 +300,12 @@ namespace RomanticWeb.JsonLd
             {
                 if (!nativeTypes)
                 {
-                    JProperty value = new JProperty(Value, @object.ToString().Replace("\"", String.Empty));
+                    var value = new JProperty(Value, @object.ToString().Replace("\"", String.Empty));
                     returnObject.Add(value);
 
                     if (@object.DataType != null)
                     {
-                        JProperty localType = new JProperty(Type, @object.DataType.ToString().Replace("\"", String.Empty));
+                        var localType = new JProperty(Type, @object.DataType.ToString().Replace("\"", String.Empty));
                         returnObject.Add(localType);
                     }
                 }
@@ -285,33 +313,33 @@ namespace RomanticWeb.JsonLd
                 {
                     if (@object.DataType != null)
                     {
-                        object objectValue = new object();
+                        object objectValue;
                         if (@object.DataType.ToString() == Xsd.Boolean.ToString())
                         {
-                            objectValue = Convert.ToBoolean(@object.Literal.ToString());
-                            JProperty value = new JProperty(Value, objectValue);
+                            objectValue = Convert.ToBoolean(@object.Literal);
+                            var value = new JProperty(Value, objectValue);
                             returnObject.Add(value);
                         }
                         else if (@object.DataType.ToString() == Xsd.Integer.ToString())
                         {
-                            objectValue = Convert.ToInt64(@object.Literal.ToString());
-                            JProperty value = new JProperty(Value, objectValue);
+                            objectValue = Convert.ToInt64(@object.Literal);
+                            var value = new JProperty(Value, objectValue);
                             returnObject.Add(value);
                         }
                         else if (@object.DataType.ToString() == Xsd.Double.ToString())
                         {
                             double doubleVal;
-                            double.TryParse(@object.Literal.ToString(), System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out doubleVal).ToString();
+                            double.TryParse(@object.Literal, System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out doubleVal);
                             objectValue = doubleVal;
-                            JProperty value = new JProperty(Value, objectValue);
+                            var value = new JProperty(Value, objectValue);
                             returnObject.Add(value);
                         }
                         else
                         {
                             objectValue = @object.ToString().Replace("\"", String.Empty);
-                            JProperty value = new JProperty(Value, objectValue);
+                            var value = new JProperty(Value, objectValue);
                             returnObject.Add(value);
-                            JProperty localType = new JProperty(Type, @object.DataType.ToString());
+                            var localType = new JProperty(Type, @object.DataType.ToString());
                             returnObject.Add(localType);
                         }
                     }
@@ -319,7 +347,7 @@ namespace RomanticWeb.JsonLd
 
                 if (@object.Language != null)
                 {
-                    JProperty language = new JProperty(Language, @object.Language.ToString().Replace("\"", String.Empty));
+                    var language = new JProperty(Language, @object.Language.Replace("\"", String.Empty));
                     returnObject.Add(language);
                 }
             }

34c74cc0a8a9762ffb20468c4fd646abb796ee41
tpluscode <tomasz@t-code.pl>
2014-08-21 08:55:48 +0200

    more culture-dependent tests

--- Tests/RomanticWeb.Tests/Converters/DoubleConverterTests.cs
+++ Tests/RomanticWeb.Tests/Converters/DoubleConverterTests.cs
@@ -1,15 +1,16 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Moq;
 using NUnit.Framework;
 using RomanticWeb.Converters;
 using RomanticWeb.Model;
+using RomanticWeb.Tests.Helpers;
 using RomanticWeb.Vocabularies;
 
 namespace RomanticWeb.Tests.Converters
 {
     [TestFixture]
-    [Culture("pl,en")]
     public class DoubleConverterTests : XsdConverterTestsBase<DoubleConverter>
     {
         protected override IEnumerable<Uri> SupportedXsdTypes
@@ -21,21 +22,31 @@ namespace RomanticWeb.Tests.Converters
             }
         }
 
-        [TestCase("+INF", double.PositiveInfinity)]
-        [TestCase("INF", double.PositiveInfinity)]
-        [TestCase("-INF", double.NegativeInfinity)]
-        [TestCase("NaN", double.NaN)]
-        [TestCase("0", 0)]
-        [TestCase("2.12", 2.12d)]
-        [TestCase("2e10", 2e10d)]
-        public void Should_convert_values_from_literals(string literal, double expectedValue)
+        [Test, Combinatorial]
+        public void Should_convert_values_from_literals(
+            [ValueSource("ConverterTestCases")] Tuple<string, double> pair, 
+            [Values("pl", "en")] string culture)
         {
-            // when
-            var value = Converter.Convert(Node.ForLiteral(literal), new Mock<IEntityContext>().Object);
+            using (new CultureScope(culture))
+            {
+                // when
+                var value = Converter.Convert(Node.ForLiteral(pair.Item1), new Mock<IEntityContext>().Object);
 
-            // then
-            Assert.That(value, Is.InstanceOf<double>());
-            Assert.That(value, Is.EqualTo(expectedValue));
+                // then
+                Assert.That(value, Is.InstanceOf<double>());
+                Assert.That(value, Is.EqualTo(pair.Item2));
+            }
         }
+
+        private IEnumerable ConverterTestCases()
+        {
+            yield return new Tuple<string, double>("+INF", double.PositiveInfinity);
+            yield return new Tuple<string, double>("INF", double.PositiveInfinity);
+            yield return new Tuple<string, double>("-INF", double.NegativeInfinity);
+            yield return new Tuple<string, double>("NaN", double.NaN);
+            yield return new Tuple<string, double>("0", 0);
+            yield return new Tuple<string, double>("2.12", 2.12d);
+            yield return new Tuple<string, double>("2e10", 2e10d);
+        } 
     }
 }

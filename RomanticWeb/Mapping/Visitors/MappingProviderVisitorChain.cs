61c33abcee040a945dd25ca3e5a5e0ae7f68c0fd
tpluscode <tomasz@t-code.pl>
2014-08-26 09:55:24 +0200

    mapping provider visitor chain

---
+++ RomanticWeb/Mapping/Visitors/MappingProviderVisitorChain.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace RomanticWeb.Mapping.Visitors
+{
+    internal class MappingProviderVisitorChain : IMappingProviderVisitorChain
+    {
+        private readonly IList<Type> _visitors = new List<Type>();
+
+        public IEnumerable<Type> Visitors
+        {
+            get
+            {
+                return new ReadOnlyCollection<Type>(_visitors);
+            }
+        }
+
+        public void AddFirst<T>() where T : IMappingProviderVisitor
+        {
+            _visitors.Insert(0, typeof(T));
+        }
+
+        public void AddLast<T>() where T : IMappingProviderVisitor
+        {
+            _visitors.Add(typeof(T));
+        }
+
+        public void AddAfter<TExisting, TNew>() where TExisting : IMappingProviderVisitor where TNew : IMappingProviderVisitor
+        {
+            var indexOfExisting = _visitors.IndexOf(typeof(TExisting));
+            if (indexOfExisting == -1)
+            {
+                AddLast<TNew>();
+            }
+
+            _visitors.Insert(indexOfExisting + 1, typeof(TNew));
+        }
+    }
+}

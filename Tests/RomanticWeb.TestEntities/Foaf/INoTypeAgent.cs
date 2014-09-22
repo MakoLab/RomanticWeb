34f2f18439d6979796f2428a1f81698820ca3d18
tpluscode <tomasz@t-code.pl>
2014-09-22 13:25:44 +0200

    more tests to demonstrate blank node problems

---
+++ Tests/RomanticWeb.TestEntities/Foaf/INoTypeAgent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using RomanticWeb.Entities;
+using RomanticWeb.Mapping.Attributes;
+
+namespace RomanticWeb.TestEntities.Foaf
+{
+    public interface INoTypeAgent : IEntity
+    {
+        [Collection("foaf", "knows")]
+        IList<INoTypeAgent> Knows { get; }
+
+        [Collection("foaf", "knows")]
+        ICollection<INoTypeAgent> KnowsCollection { get; }
+
+        [Property("foaf", "givenName")]
+        string Name { get; set; }
+    }
+}

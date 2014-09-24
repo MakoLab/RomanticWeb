cf0a2847d1d4616a04598e7f0330b492b771481f
tpluscode <tomasz@t-code.pl>
2014-09-24 10:10:47 +0200

    nuspecs

--- RomanticWeb/Properties/AssemblyInfo.cs
+++ RomanticWeb/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using Anotar.NLog;
 
 [assembly: AssemblyTitle("RomanticWeb")]
-[assembly: AssemblyDescription("Relation-Object Mapping for the Semantic Web")]
+[assembly: AssemblyDescription("RDF-Object Mapping for the Semantic Web")]
 
 [assembly: InternalsVisibleTo("RomanticWeb.Tests")]
 [assembly: InternalsVisibleTo("ImpromptuInterfaceDynamicAssembly")]
